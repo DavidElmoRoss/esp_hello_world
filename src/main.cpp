4758d9c1488fb452bd2cbc4de7d11e2a9b0dbc1b
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2025-02-07 14:35:02 -0500

    Update main.cpp
    Updated comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,17 @@
 * Author      : David Elmo Ross
 * Modification: 2025/01/11
 **********************************************************************/
-#include <Arduino.h>
-
+#include <Arduino.h>             // include Arduino.h
 
 void setup() 
 {
-
-
-  Serial.begin(115200);
- 
-}
+  Serial.begin(115200);          // set serial rate to 115200
+}                                // make sure that platformio.ini uses the same value
 
 void loop() 
 {
-  printf("Hello World");
-  fflush(stdout);
-   for(;;); 
-   
+  printf("Hello World");         // send Hello World to the stdout buffer
+  fflush(stdout);                // flush stdout buffer to screen
+   for(;;);                      // stay in endless loop
 }
 
